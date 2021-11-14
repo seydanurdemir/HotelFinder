cc2fac232f0176560aef4dfe9011b5f0ec5a320e
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-15 00:46:45 +0300

    Implement HotelManager class, Add (sample) business rules, Add comments

--- HotelFinder.Business/Concrete/HotelManager.cs
+++ HotelFinder.Business/Concrete/HotelManager.cs
@@ -1,4 +1,6 @@
 ﻿using HotelFinder.Business.Abstract;
+using HotelFinder.DataAccess.Abstract;
+using HotelFinder.DataAccess.Concrete;
 using HotelFinder.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,29 +12,50 @@ namespace HotelFinder.Business.Concrete
 {
     public class HotelManager : IHotelService
     {
+        private IHotelRepository _hotelRepository;
+        public HotelManager()
+        {
+            // add business rules
+            _hotelRepository = new HotelRepository(); // will be changed when adding dependency injection
+        }
         public Hotel CreateHotel(Hotel hotel)
         {
-            throw new NotImplementedException();
+            // add business rules
+            return _hotelRepository.CreateHotel(hotel);
         }
 
         public void DeleteHotel(int id)
         {
-            throw new NotImplementedException();
+            // add business rules
+            // sample business rule addition
+            if (id < 1)
+            {
+                throw new Exception("id can not be less than 1");
+            }
+            _hotelRepository.DeleteHotel(id);
         }
 
         public List<Hotel> GetAllHotels()
         {
-            throw new NotImplementedException();
+            // add business rules
+            return _hotelRepository.GetAllHotels();
         }
 
         public Hotel GetHotelById(int id)
         {
-            throw new NotImplementedException();
+            // add business rules
+            // sample business rule addition
+            if (id < 1)
+            {
+                throw new Exception("id can not be less than 1");
+            }
+            return _hotelRepository.GetHotelById(id);
         }
 
         public Hotel UpdateHotel(Hotel hotel)
         {
-            throw new NotImplementedException();
+            // add business rules
+            return _hotelRepository.UpdateHotel(hotel);
         }
     }
 }
