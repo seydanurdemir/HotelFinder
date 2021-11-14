e19efe8f3414da0cc93ca744a07e9eb30a3bb8a9
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-14 23:49:01 +0300

    Add EF Core Tools package, Add migration initialCreate, Update database, Edit connection string

--- HotelFinder.DataAccess/HotelDbContext.cs
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -13,7 +13,15 @@ namespace HotelFinder.DataAccess
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Data Source=localhost/SQLEXPRESS;Database=HotelDb;Integrated Security=True");
+
+            //optionsBuilder.UseSqlServer("Server=localhost/SQLEXPRESS;Database=HotelDb;Trusted_Connection=True;"); // copied, did not try
+            //optionsBuilder.UseSqlServer("Data Source=localhost/SQLEXPRESS;Database=HotelDb;Integrated Security=True"); // error : do not use "/"
+            //optionsBuilder.UseSqlServer("Data Source=localhost\SQLEXPRESS;Database=HotelDb;Integrated Security=True"); // error : use "\\" or "@"
+            //optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Database=HotelDb;Integrated Security=True"); // run : but did not do this way
+            //optionsBuilder.UseSqlServer(@"Data Source=localhost\SQLEXPRESS;Database=HotelDb;Integrated Security=True"); // run : do this way, just define string
+
+            string connectionString = @"Data Source=localhost\SQLEXPRESS;Database=HotelDb;Integrated Security=True";
+            optionsBuilder.UseSqlServer(connectionString);
         }
 
         public DbSet<Hotel> Hotels { get; set; }
