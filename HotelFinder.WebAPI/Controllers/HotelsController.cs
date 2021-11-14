1c2213d56e7a57964d48a22ab31b4160d80f6c21
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-15 01:22:20 +0300

    Implement partially HotelsController, Delete WeatherForecast, Change launchSettings.json by changing url to "api/Hotels"

--- HotelFinder.WebAPI/Controllers/HotelsController.cs
+++ HotelFinder.WebAPI/Controllers/HotelsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.Business.Concrete;
+using HotelFinder.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +14,23 @@ namespace HotelFinder.WebAPI.Controllers
     [ApiController]
     public class HotelsController : ControllerBase
     {
+        private IHotelService _hotelService;
+        
+        public HotelsController()
+        {
+            _hotelService = new HotelManager(); // will be changed when adding dependency injection
+        }
+
+        [HttpGet]
+        public List<Hotel> Get()
+        {
+            return _hotelService.GetAllHotels();
+        }
+
+        [HttpGet("{id}")]
+        public Hotel GetById(int id)
+        {
+            return _hotelService.GetHotelById(id);
+        }
     }
 }
