cc492f51815d52b5cec05bc0b52df175e2f5894c
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-15 00:01:29 +0300

    Add abstract IHotelRepository interface, Add concrete HotelRepository that inherits IHotelRepository interface

---
+++ HotelFinder.DataAccess/Abstract/IHotelRepository.cs
@@ -0,0 +1,18 @@
+﻿using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess.Abstract
+{
+    public interface IHotelRepository
+    {
+        List<Hotel> GetAllHotels(); // Database Read (Select *) = Rest Http Get
+        Hotel GetHotelById(int id); // Database Read (Select * Where Id=id) = Rest Http Get {id}
+        Hotel CreateHotel(Hotel hotel); // Database Create (Insert) = Rest Http Post {json}
+        Hotel UpdateHotel(Hotel hotel); // Database Update (Update) = Rest Http Put {json}
+        void DeleteHotel(int id); // Database Delete (Delete) = Rest Http Delete {id}
+    }
+}
