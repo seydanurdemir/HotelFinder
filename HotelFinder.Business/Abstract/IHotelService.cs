903d217b7ad6e4dfaad97a455000ad65e7e86b5a
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-15 00:19:31 +0300

    Add Business project, Add abstract IHotelService interface, Add concrete HotelService class

---
+++ HotelFinder.Business/Abstract/IHotelService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.Business.Abstract
+{
+    public interface IHotelService
+    {
+    }
+}
---
+++ HotelFinder.Business/Concrete/HotelService.cs
@@ -0,0 +1,13 @@
+﻿using HotelFinder.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.Business.Concrete
+{
+    public class HotelService : IHotelService
+    {
+    }
+}
