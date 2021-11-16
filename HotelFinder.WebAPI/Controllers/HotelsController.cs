66e9ba2e12bd23fbd0eb03e69e1a3c71ce4d78a0
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-16 23:58:15 +0300

    Complete API calls Swagger documentation resolve #2

--- HotelFinder.WebAPI/Controllers/HotelsController.cs
+++ HotelFinder.WebAPI/Controllers/HotelsController.cs
@@ -21,30 +21,53 @@ namespace HotelFinder.WebAPI.Controllers
             _hotelService = hotelService; // dependency injection
         }
 
+        /// <summary>
+        /// Gets all hotels.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public List<Hotel> Get()
         {
             return _hotelService.GetAllHotels();
         }
 
+        /// <summary>
+        /// Gets hotel by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public Hotel GetById(int id)
         {
             return _hotelService.GetHotelById(id);
         }
 
+        /// <summary>
+        /// Create hotel.
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
         [HttpPost]
         public Hotel Post([FromBody]Hotel hotel)
         {
             return _hotelService.CreateHotel(hotel);
         }
 
+        /// <summary>
+        /// Update the hotel from body.
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
         [HttpPut]
         public Hotel Put([FromBody]Hotel hotel)
         {
             return _hotelService.UpdateHotel(hotel);
         }
 
+        /// <summary>
+        /// Delete the hotel by id.
+        /// </summary>
+        /// <param name="id"></param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
